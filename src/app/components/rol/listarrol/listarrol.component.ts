b3664bee004e971429c2ee624dfe499a5331746d
joeCQ2000 <joelcamargoquispe4asec@gmail.com>
2024-11-15 19:14:22 -0500

    Botón de eliminación lógica

--- src/app/components/rol/listarrol/listarrol.component.ts
+++ src/app/components/rol/listarrol/listarrol.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Rol } from '../../../models/Rol';
-import { RolService } from '../../../services/rol.service';
-import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatCardModule } from '@angular/material/card';
+import { RolService } from '../../../services/rol.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-listarrol',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatPaginator, MatIconModule, RouterModule],
+  imports: [ CommonModule,
+    MatCardModule,
+    MatPaginator,
+    MatIconModule,
+    RouterModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSort,],
   templateUrl: './listarrol.component.html',
   styleUrl: './listarrol.component.css'
 })
 export class ListarrolComponent implements OnInit {
-  dataSource: Rol[] = [];
+  dataSource: MatTableDataSource<Rol> = new MatTableDataSource();
   displayedColumns: string [] = ['Id', 'Name', 'UserId'];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -23,28 +36,66 @@ export class ListarrolComponent implements OnInit {
   pageSize = 3;
   currentPage = 0;
   pagedData: Rol[] = [];
-  constructor(private rS: RolService) {}
+  constructor(private rS: RolService, private snackBar: MatSnackBar) {}
   ngOnInit(): void {
-    this.rS.list().subscribe((data) => {
-      {
-       this.dataSource = data;
-      this.updatePagedData();
-      }
-    });
     this.rS.getList().subscribe((data) => {
-      this.dataSource = data;
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
       this.updatePagedData();
+    });
+    this.rS.list().subscribe((data) => {
+      this.rS.setList(data);
     });
   }
-    updatePagedData(): void {
-      const startIndex = this.currentPage * this.pageSize;
-      const endIndex = startIndex + this.pageSize;
-      this.pagedData = this.dataSource.slice(startIndex, endIndex);
-    }
+  updatePagedData(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedData = this.dataSource.data.slice(startIndex, endIndex);  
+  }
+  
     onPageChange(event: PageEvent): void {
       this.pageSize = event.pageSize;
       this.currentPage = event.pageIndex;
       this.updatePagedData();
     }
-  
+    eliminar(id: number): void {
+      this.rS.delete(id).subscribe({
+        next: () => {
+
+          this.dataSource.data = this.dataSource.data.filter((rol) => rol.idRol !== id);
+          this.updatePagedData(); 
+    
+          this.snackBar.open('Registro eliminado exitosamente', 'Cerrar', {
+            duration: 2000,
+            verticalPosition: 'bottom',
+            horizontalPosition: 'center',
+          });
+        },
+        error: (err) => {
+
+          if (err.status === 500) {
+            this.snackBar.open(
+              'No se puede eliminar porque este registro ya existe',
+              'Cerrar',
+              {
+                duration: 2000,
+                verticalPosition: 'bottom',
+                horizontalPosition: 'center',
+              }
+            );
+          } else {
+            this.snackBar.open(
+              'Ocurrió un error al intentar eliminar el registro',
+              'Cerrar',
+              {
+                duration: 2000,
+                verticalPosition: 'bottom',
+                horizontalPosition: 'center',
+              }
+            );
+          }
+        },
+      });
+    }
+    
 }
