2dbacfdb867617374053f640d88386b56c8830ec
joeCQ2000 <joelcamargoquispe4asec@gmail.com>
2024-11-16 01:04:47 -0500

    Creación del Acerca de nosotros diseño

--- src/app/components/landing-page/acerca-de-nosotros/acerca-de-nosotros.component.ts
+++ src/app/components/landing-page/acerca-de-nosotros/acerca-de-nosotros.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-acerca-de-nosotros',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './acerca-de-nosotros.component.html',
   styleUrl: './acerca-de-nosotros.component.css'
 })
