6b2026575f210c67bd53c9ee7f6607d7c8dababb
alx-2023 <u202218649@upc.edu.pe>
2024-11-17 15:05:35 -0500

    DiagnosticoxTratamiento + home visual

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  username: string = '';
+  rutaHome: boolean = false;
 
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  ngOnInit() {
+    this.username = this.loginService.showUsername();
+    this.rutaHome = this.router.url === '/homes';
+
+    if (!sessionStorage.getItem('pageReloaded')) {
+      sessionStorage.setItem('pageReloaded', 'true');
+      location.reload();
+    }
+  }
 }
